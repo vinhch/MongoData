4422ac0c47f7236536ee518507ee11dff11cc076
vinhomn <vinhomn@gmail.com>
2017-01-24 10:45:12 +0700

    add console test project

---
+++ src/MongoData.ConsoleTests/AppStartup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using MongoData.Repository;
+
+namespace MongoData.ConsoleTests
+{
+    public static class AppStartup
+    {
+        public static IConfigurationRoot Configuration { get; private set; }
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public static void Run()
+        {
+            Startup();
+            ConfigureServices();
+        }
+
+        private static void Startup()
+        {
+            if (Configuration == null)
+            {
+                // get appsettings
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+                Configuration = builder.Build();
+            }
+        }
+
+        /// <summary>
+        /// Config .Net Core built in Dependency Injection
+        /// </summary>
+        private static void ConfigureServices()
+        {
+            if (ServiceProvider != null) return;
+
+            var services = new ServiceCollection()
+                .AddScoped<IUnitOfWork>(provider => new UnitOfWork(Configuration.GetConnectionString("DefaultConnection")))
+                .AddScoped<ICategoryRepository, CategoryRepository>()
+                .AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<TestDb>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
---
+++ src/MongoData.ConsoleTests/TestDb.cs
@@ -0,0 +1,61 @@
+﻿using MongoData.Domain;
+using MongoData.Repository;
+
+namespace MongoData.ConsoleTests
+{
+    public class TestDb
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public TestDb(IUnitOfWork unitOfWork,
+            ICategoryRepository categoryRepository,
+            IProductRepository productRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+
+        public void Run()
+        {
+            Setup();
+            Test();
+            Cleanup();
+        }
+
+        private void Setup()
+        {
+            DropDb();
+        }
+
+        private void Cleanup()
+        {
+            DropDb();
+        }
+
+        private void DropDb()
+        {
+            _unitOfWork.Client.DropDatabase(_unitOfWork.Database.DatabaseNamespace.DatabaseName);
+        }
+
+        private void Test()
+        {
+            var product = new Product
+            {
+                Name = "test",
+                Type = ProductTypes.Product
+            };
+            product = _productRepository.Add(product);
+
+            var count = _productRepository.Count();
+            var product1 = _productRepository.GetById(product.Id);
+
+            product.Name = "test_1";
+            product = _productRepository.Update(product);
+
+            var product2 = _productRepository.GetById(product.Id);
+        }
+    }
+}
