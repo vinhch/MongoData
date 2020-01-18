235b0c2126c0922a83f9d0eb3c47b867b00fc04f
VinhCao <vinhomn@gmail.com>
2020-01-19 05:31:06 +0700

    Add more xml comments, add some async methods, change Repository Exists() to Any()

--- src/MongoData/Repository/IRepository.cs
+++ src/MongoData/Repository/IRepository.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using MongoDB.Driver;
 
 namespace MongoData.Repository
 {
@@ -25,35 +25,126 @@ namespace MongoData.Repository
         /// <value>The Mongo collection (to perform advanced operations).</value>
         IMongoCollection<T> Collection { get; }
 
+        /// <summary>
+        /// Returns a IQueryable<T>.
+        /// </summary>
+        /// <returns>IQueryable T.</returns>
         IQueryable<T> AsQueryable();
 
+        /// <summary>
+        /// Returns a IQueryable<T> allowing diskuse to handle large datasets.
+        /// </summary>
+        /// <returns>IQueryable T.</returns>
+        IQueryable<T> AsQueryableLargeDataSet();
+
+        /// <summary>
+        /// filter for collection
+        /// </summary>
+        FilterDefinitionBuilder<T> Filter { get; }
+
+        /// <summary>
+        /// updater for collection
+        /// </summary>
+        UpdateDefinitionBuilder<T> Updater { get; }
+
+        /// <summary>
+        /// projector for collection
+        /// </summary>
+        ProjectionDefinitionBuilder<T> Projector { get; }
+
+        /// <summary>
+        /// indexer for collection
+        /// </summary>
+        IndexKeysDefinitionBuilder<T> Indexer { get; }
+
+        /// <summary>
+        /// Returns the T by its given id.
+        /// </summary>
+        /// <param name="id">The value representing the ObjectId of the entity to retrieve.</param>
+        /// <returns>The Entity T.</returns>
         T GetById(TKey id);
 
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate, int pageIndex, int size);
+
+        /// <summary>
+        /// Adds the new entity in the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity including its new ObjectId.</returns>
         T Add(T entity);
 
+        /// <summary>
+        /// Adds the new entities in the repository.
+        /// </summary>
+        /// <param name="entities">The entities of type T.</param>
         void Add(IEnumerable<T> entities);
 
+        /// <summary>
+        /// Upserts an entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>The updated entity.</returns>
         T Update(T entity);
 
+        /// <summary>
+        /// Upserts the entities.
+        /// </summary>
+        /// <param name="entities">The entities to update.</param>
         void Update(IEnumerable<T> entities);
 
+        /// <summary>
+        /// Deletes an entity from the repository by its id.
+        /// </summary>
+        /// <param name="id">The entity's id.</param>
         void Delete(TKey id);
 
+        /// <summary>
+        /// Deletes the given entity.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
         void Delete(T entity);
 
+        /// <summary>
+        /// Deletes the entities matching the predicate.
+        /// </summary>
+        /// <param name="predicate">The expression.</param>
         void Delete(Expression<Func<T, bool>> predicate);
 
+        /// <summary>
+        /// Deletes all entities in the repository.
+        /// </summary>
         void DeleteAll();
 
         void Drop();
 
+        /// <summary>
+        /// Counts the total entities in the repository.
+        /// </summary>
+        /// <returns>Count of entities in the repository.</returns>
         long Count();
 
-        bool Exists();
+        long EstimatedCount();
 
-        bool Exists(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// Checks if the entity exists for given predicate.
+        /// </summary>
+        bool Any();
+
+        /// <summary>
+        /// Checks if the entity exists for given predicate.
+        /// </summary>
+        /// <param name="predicate">The expression.</param>
+        /// <returns>True when an entity matching the predicate exists, false otherwise.</returns>
+        bool Any(Expression<Func<T, bool>> predicate);
     }
 
+    /// <summary>
+    /// IRepository definition.
+    /// </summary>
+    /// <typeparam name="T">The type contained in the repository.</typeparam>
+    /// <remarks>Entities are assumed to use strings for Id's.</remarks>
     public interface IRepository<T> : IRepository<T, string>
         where T : IEntity<string>
     {
---
+++ src/MongoData/Repository/IRepositoryAsync.cs
@@ -0,0 +1,101 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MongoData.Repository
+{
+    public interface IRepositoryAsync<T, TKey> : IRepository<T, TKey>
+        where T : IEntity<TKey>
+    {
+        /// <summary>
+        /// Returns the T by its given id.
+        /// </summary>
+        /// <param name="id">The Id of the entity to retrieve.</param>
+        /// <returns>The Entity T.</returns>
+        Task<T> GetByIdAsync(TKey id);
+
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, int pageIndex, int size);
+
+        /// <summary>
+        /// Adds the new entity in the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity including its new ObjectId.</returns>
+        Task<T> AddAsync(T entity);
+
+        /// <summary>
+        /// Adds the new entities in the repository.
+        /// </summary>
+        /// <param name="entities">The entities of type T.</param>
+        Task AddAsync(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Upserts an entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>The updated entity.</returns>
+        Task<T> UpdateAsync(T entity);
+
+        /// <summary>
+        /// Upserts the entities.
+        /// </summary>
+        /// <param name="entities">The entities to update.</param>
+        Task UpdateAsync(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Deletes the given entity.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        Task<DeleteResult> DeleteAsync(TKey Id);
+
+        /// <summary>
+        /// Deletes the given entity.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        Task<DeleteResult> DeleteAsync(T entity);
+
+        /// <summary>
+        /// Deletes the entities matching the predicate.
+        /// </summary>
+        /// <param name="predicate">The expression.</param>
+        Task<DeleteResult> DeleteAsync(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// Deletes all entities in the repository.
+        /// </summary>
+        Task<DeleteResult> DeleteAllAsync();
+
+        Task DropAsync();
+
+        /// <summary>
+        /// Counts the total entities in the repository.
+        /// </summary>
+        /// <returns>Count of entities in the repository.</returns>
+        Task<long> CountAsync();
+
+        /// <summary>
+        /// Checks if the entity exists for given predicate.
+        /// </summary>
+        Task<bool> AnyAsync();
+
+        /// <summary>
+        /// Checks if the entity exists for given predicate.
+        /// </summary>
+        /// <param name="predicate">The expression.</param>
+        /// <returns>True when an entity matching the predicate exists, false otherwise.</returns>
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    }
+
+    /// <summary>
+    /// IRepository definition.
+    /// </summary>
+    /// <typeparam name="T">The type contained in the repository.</typeparam>
+    /// <remarks>Entities are assumed to use strings for Id's.</remarks>
+    public interface IRepositoryAsync<T> : IRepositoryAsync<T, string>
+        where T : IEntity<string>
+    { }
+}
